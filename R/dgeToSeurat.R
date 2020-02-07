c9f4fe1ca5fb04f4d8ef26c1bada75c7ec0c0af8
Dylan <dylanmcable@gmail.com>
2020-02-06 17:33:28 -0800

    fixed DGEToSeurat

--- R/dgeToSeurat.R
+++ R/dgeToSeurat.R
@@ -1,7 +1,6 @@
-dgeToSeurat <- function() {
-  mydir <- "/Users/dcable/Documents/MIT/Research/Rafalab/Projects/slideseq/Cell Demixing"
-  refdir <- paste(mydir,"Data/KidneyHumanReference",sep="/")
-
+#input: refdir, the directory of the reference
+#e.g. dgeToSeurat('Data/Reference/KidneyHumanReference')
+dgeToSeurat <- function(refdir) {
   raw.data = t(read.csv(file.path(refdir,"dge.csv")))
   meta_data = read.csv(file.path(refdir,"meta_data.csv"))
   rownames(meta_data) = meta_data$barcode
@@ -12,6 +11,6 @@ dgeToSeurat <- function() {
   cell_dict_file <- paste(refdir,"cell_type_dict.csv",sep="/")
   true_type_names <- remap_celltypes(cell_dict_file, meta_data$cluster)
   meta_data$liger_ident_coarse = true_type_names
-  reference = CreateSeuratObject(raw.data, meta.data = meta_data)
+  reference = Seurat::CreateSeuratObject(raw.data, meta.data = meta_data)
   saveRDS(reference, paste(refdir,"SCRef.RDS",sep="/"))
 }
