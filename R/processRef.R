3f8aea90424f51fc6ebc01b96422300ddca14e37
Dylan <dylanmcable@gmail.com>
2020-02-11 12:03:28 -0500

    reference can not have all genes

--- R/processRef.R
+++ R/processRef.R
@@ -1,4 +1,4 @@
-get_cell_type_info <- function(raw.data, cell_types, cell_type_names = NULL) {
+get_cell_type_info <- function(raw.data, cell_types, nUMI, cell_type_names = NULL) {
   if(is.null(cell_type_names))
     cell_type_names = levels(cell_types)
 
@@ -6,7 +6,8 @@ get_cell_type_info <- function(raw.data, cell_types, cell_type_names = NULL) {
 
   get_cell_mean <- function(cell_type) {
     cell_type_data = raw.data[,cell_types == cell_type]
-    normData = sweep(cell_type_data,2,colSums(cell_type_data),`/`)
+    cell_type_umi = nUMI[cell_types == cell_type]
+    normData = sweep(cell_type_data,2,cell_type_umi,`/`)
     return(rowSums(normData) / dim(normData)[2])
   }
 
