d1427b61cfaa2c5370d0418e56d004310f921655
Dylan <dylanmcable@gmail.com>
2020-02-10 20:41:50 -0500

    downsample

--- downsamplePuck.R
+++ downsamplePuck.R
@@ -3,3 +3,6 @@ library(Matrix)
 config <- config::get()
 slideseqdir <- file.path("Data/Slideseq",config$slideseqfolder)
 counts <- readr::read_csv(file = paste(slideseqdir,config$puckfile,sep="/"))
+N_keep = 1000
+restr_counts <- counts[,1:N_keep]
+readr::write_csv(restr_counts, paste(slideseqdir,"smallDGE.csv",sep="/"))
