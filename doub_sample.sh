9ad811b7934b9c845742145845a06efa71892c77
Dylan <dylanmcable@gmail.com>
2020-05-03 18:37:05 -0400

    cleanup

--- doub_sample.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source /broad/software/scripts/useuse
-reuse -q R-3.5
-cd /YOUR_PATH/RCTD/
-set -e
-mkdir -p logs
-mkdir -p logs/callDoublets/
-Rscript callDoublets.R ${SGE_TASK_ID} > logs/callDoublets/chunk-${SGE_TASK_ID}.txt
--- fitBulk.R
+++
@@ -1,5 +0,0 @@
-library(RCTD)
-library(Matrix)
-iv <- init_RCTD(gene_list_reg = F, get_proportions = F, load_info=F) #initial variables
-bulkResults <- fitBulk(iv)
-
