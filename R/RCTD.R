dc7b7df2ce9a33d59bfa22d11b01bdd2a2b8442c
Dylan Cable <40443769+dmcable@users.noreply.github.com>
2021-09-24 14:25:00 -0400

    Update create.RCTD for configurable MAX_MULTI_TYPES

--- R/RCTD.R
+++ R/RCTD.R
@@ -37,10 +37,10 @@ create.RCTD <- function(spatialRNA, reference, max_cores = 8, test_mode = FALSE,
                          class_df = NULL, CELL_MIN_INSTANCE = 25, cell_type_names = NULL) {
 
    config <- list(gene_cutoff = gene_cutoff, fc_cutoff = fc_cutoff, gene_cutoff_reg = gene_cutoff_reg, fc_cutoff_reg = fc_cutoff_reg, UMI_min = UMI_min, UMI_min_sigma = UMI_min_sigma, max_cores = max_cores,
-                 N_epoch = 8, N_X = 50000, K_val = 100, N_fit = 1000, N_epoch_bulk = 30, MIN_CHANGE_BULK = 0.0001, MIN_CHANGE_REG = 0.001, UMI_max = UMI_max, MIN_OBS = 3)
+                 N_epoch = 8, N_X = 50000, K_val = 100, N_fit = 1000, N_epoch_bulk = 30, MIN_CHANGE_BULK = 0.0001, MIN_CHANGE_REG = 0.001, UMI_max = UMI_max, MIN_OBS = 3, MAX_MULTI_TYPES = 4)
    if(test_mode)
      config <- list(gene_cutoff = .00125, fc_cutoff = 0.5, gene_cutoff_reg = 0.002, fc_cutoff_reg = 0.75, UMI_min = 1000,
-          N_epoch = 1, N_X = 50000, K_val = 100, N_fit = 50, N_epoch_bulk = 4, MIN_CHANGE_BULK = 1, MIN_CHANGE_REG = 0.001, UMI_max = 200000, MIN_OBS = 3, max_cores = 1, UMI_min_sigma = 300)
+          N_epoch = 1, N_X = 50000, K_val = 100, N_fit = 50, N_epoch_bulk = 4, MIN_CHANGE_BULK = 1, MIN_CHANGE_REG = 0.001, UMI_max = 200000, MIN_OBS = 3, max_cores = 1, UMI_min_sigma = 300, MAX_MULTI_TYPES = 4)
    if(is.null(cell_type_names))
       cell_type_names <- levels(reference@cell_types)
    cell_type_info <- list(info = process_cell_type_info(reference, cell_type_names = cell_type_names, CELL_MIN = CELL_MIN_INSTANCE), renorm = NULL)
