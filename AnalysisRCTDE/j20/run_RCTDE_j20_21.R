6c25d42807e02ed8f5289753b087e554aea4c04e
Dylan Cable <dylanmcable@gmail.com>
2021-12-14 16:26:41 -0500

    add j20 analysis files

---
+++ AnalysisRCTDE/j20/run_RCTDE_j20_21.R
@@ -0,0 +1,14 @@
+library(Matrix)
+library(RCTD)
+library(doParallel)
+library(ggplot2)
+
+datadir <- '../RCTD/data/SpatialRNA/Puck_210605/21'
+myRCTD<- readRDS(file.path(datadir,'myRCTD_j20.rds'))
+load(file = file.path(datadir,'exvar_thresh.RData')) # exvar, explanatory_variable
+aggregate_cell_types(myRCTD, names(exvar))
+cell_types <- c("Astrocyte","CA1","CA3", "Denate", "Interneuron" ,  "Microglia_Macrophages", "Oligodendrocyte" )
+myRCTD@config$max_cores <- 2
+myRCTD <- run.de.single(myRCTD,exvar, datadir = datadir, cell_types = cell_types) #testing gene_threshold = .001
+saveRDS(myRCTD, file.path(datadir, 'myRCTDde_thresh.rds'))
+#make_all_de_plots(myRCTD, datadir)
